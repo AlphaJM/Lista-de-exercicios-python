1bb1e2dde1b677492219f8f2760c970cdf230f9e
João Marcello <jmpp13@outlook.com>
2023-07-05 16:25:31 -0300

    Começo de uma lista de exercícios de python.

---
+++ ex1.py
@@ -0,0 +1,14 @@
+#ex1
+#print('Olá mundo')
+
+#ex2
+#num = input('Escreva um número:')
+
+#print('O número escrito foi:', num)
+
+#ex3
+print('Bem vindo a calculadora de soma')
+num1 = input('Digite o primeiro número:')
+num2 = input('Digite o segundo número:')
+
+print('O resultado da soma foi:', int(num1) + int(num2))
